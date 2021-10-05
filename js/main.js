a3026a6d1ba1aae1c5be23198c4ecc5d1d58cf8b
grund1k <dfyz8607@gmail.com>
2021-10-05 11:48:29 +0300

    Исправление функций из первого задания

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
-function getRandomIntInclusive(min, max) {
-  if (min < 0 || max < 0) {
-    return  'Значения диапозона должны быть больше 0';
-  }
-
+function getRandomIntInclusive(min = 10, max = 1000) {
   if (min === max) {
     return min;
   }
 
-  if (min > max) {
-    const tmp = min;
-    min = max;
-    max = tmp;
-  }
+  min = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  max = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 getRandomIntInclusive(100, 1000);
 
-function commentLenghtCheck(comment, maxCommentLength) {
-  return comment.length < maxCommentLength;
+function commentLenghtCheck(comment = 'Какой-то комментария', maxCommentLength = 140) {
+  return comment.length <= maxCommentLength;
 }
 
 commentLenghtCheck('Универсалии могут существовать как сущности конкретных вещей, или они являются лишь порождениями разума?', 140);
